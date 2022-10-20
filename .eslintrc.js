dc097cdb0fda903fcf803c54266015349afe80a3
guotingting <1531297152@qq.com>
2022-10-21 01:13:00 +0800

    feat: Test if eslint works

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   rules: {
     ...base.rules,
     'arrow-parens': 0,
-    'react/no-array-index-key': 0,
+    // 'react/no-array-index-key': 0,
     'react/sort-comp': 0,
     '@typescript-eslint/no-explicit-any': 1,
     '@typescript-eslint/no-empty-interface': 1,
