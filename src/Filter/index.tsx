db710c0bea82258213aff5b0ae1ce475ad0a208b
guotingting <1531297152@qq.com>
2022-11-19 18:36:00 +0800

    feat: change filter icon

--- src/Filter/index.tsx
+++ src/Filter/index.tsx
@@ -3,12 +3,12 @@ import { usePopper } from 'react-popper';
 import classnames from 'classnames';
 import Checkbox from '../Checkbox';
 import Radio from '../Radio';
-import FilterIcon from '@/assets/filter.svg';
-import FilterActiveIcon from '@/assets/filter-active.svg';
-import QueryIcon from '@/assets/query.svg';
-import EmptyIcon from '@/assets/empty.svg';
+import Icon from '../Icon';
+import { ReactComponent as FilterIcon } from '@/assets/filter.svg';
+import { ReactComponent as QueryIcon } from '@/assets/query.svg';
+import { ReactComponent as EmptyIcon } from '@/assets/empty.svg';
 import type { FilterMenusType } from '../interface';
-import styles from './index.less';
+import './index.less';
 
 interface FilterProps {
   filters: FilterMenusType[];
@@ -122,37 +122,44 @@ const Filter = (props: FilterProps) => {
 
   return (
     <>
-      <div className={styles.filterContainer} ref={setReferenceElement} onClick={handleClick}>
+      <div className="filter-container" ref={setReferenceElement} onClick={handleClick}>
         {typeof filterIcon === 'function' ? (
           filterIcon(!!filteredValue.length)
         ) : (
-          <img
-            src={filteredValue.length ? FilterActiveIcon : FilterIcon}
-            alt="filter-icon"
-            className={styles.filterIcon}
+          <Icon
+            component={FilterIcon}
+            className={classnames({
+              'filter-icon': true,
+              'filter-icon-active': filteredValue.length,
+            })}
           />
         )}
       </div>
       <div
         ref={setPopperElement}
         className={classnames({
-          [styles.popper]: true,
-          [styles.popperShow]: visible,
-          [styles.popperHidden]: !visible,
+          'filter-popper': true,
+          'filter-popper-show': visible,
+          'filter-popper-hidden': !visible,
         })}
         style={popperStyle.popper}
         {...attributes.popper}
       >
         {!!filterSearch && filters.length ? (
-          <div className={styles.filterSearch}>
-            <div className={classnames({ [styles.inputWrap]: true, [styles.focused]: isFocus })}>
-              <div className={styles.inputPrefix}>
-                <img src={QueryIcon} alt="query" />
+          <div className="filter-search">
+            <div
+              className={classnames({
+                'search-input-wrap': true,
+                'search-input-wrap-focus': isFocus,
+              })}
+            >
+              <div className="input-prefix">
+                <Icon component={QueryIcon} />
               </div>
               <input
                 type="text"
                 placeholder="在筛选项中搜索"
-                className={styles.searchInput}
+                className="search-input"
                 onFocus={handleFocus}
                 onBlur={handleBlur}
                 onChange={handleSearch}
@@ -160,21 +167,21 @@ const Filter = (props: FilterProps) => {
             </div>
           </div>
         ) : null}
-        <div className={styles.filterList}>
+        <div className="filter-list">
           {!filters.length && (
-            <div className={styles.emptySection}>
-              <img src={EmptyIcon} alt="empty" />
+            <div className="filter-empty">
+              <Icon component={EmptyIcon} className="filter-empty-icon" />
               <span>No filters</span>
             </div>
           )}
-          {!filterOptions.length && !!filterSearch && (
-            <div className={styles.emptySection}>
+          {filters.length > 0 && !filterOptions.length && !!filterSearch && (
+            <div className="filter-empty">
               <span>Not Found</span>
             </div>
           )}
           {filterOptions.map((f) => {
             return (
-              <div className={styles.filterItem} key={f.value}>
+              <div className="filter-item" key={f.value}>
                 {filterMultiple ? (
                   <Checkbox
                     checked={checkedValue.indexOf(f.value) >= 0}
@@ -198,11 +205,11 @@ const Filter = (props: FilterProps) => {
             );
           })}
         </div>
-        <div className={styles.filterFooter}>
+        <div className="filter-footer">
           <div
             className={classnames({
-              [styles.filterBtn]: true,
-              [styles.disabled]: !checkedValue.length,
+              'filter-btn': true,
+              'filter-btn-disabled': !checkedValue.length,
             })}
             onClick={handleFilter}
           >
@@ -210,8 +217,8 @@ const Filter = (props: FilterProps) => {
           </div>
           <div
             className={classnames({
-              [styles.filterBtn]: true,
-              [styles.disabled]: !checkedValue.length,
+              'filter-btn': true,
+              'filter-btn-disabled': !checkedValue.length,
             })}
             onClick={handleReset}
           >
