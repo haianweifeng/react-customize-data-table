4b8794f0c1b381aaa19caa86675c292d7848ad66
guotingting <1531297152@qq.com>
2022-12-04 23:29:36 +0800

    feat: update the demo about the SortRender feat: modify style

--- docs/demo/SortRender.tsx
+++ docs/demo/SortRender.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { ColumnsType } from 'react-data-table';
-import { Table } from 'react-data-table';
+import { Table, Icon } from 'react-data-table';
 
 interface DataType {
   key: React.Key;
@@ -56,11 +56,24 @@ const data: DataType[] = [
 ];
 
 const App = () => {
-  const commonStyle = {
-    height: '12px',
-    transform: 'rotate(-90deg) scale(0.8, 1.2)',
-    cursor: 'pointer',
-  };
+  const UpArrow = () => (
+    <svg viewBox="0 0 1024 1024" width="1em" height="1em">
+      <path
+        d="M483.029333 286.165333l30.165334-30.208 415.957333 415.829334c16.426667 16.426667 16.64 43.648 0 60.288a42.538667 42.538667 0 0 1-60.330667 0.042666L513.28 376.746667l-355.242667 355.413333a42.496 42.496 0 0 1-60.288 0 42.837333 42.837333 0 0 1-0.085333-60.330667l383.701333-383.872 1.706667-1.749333z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+
+  const DownArrow = () => (
+    <svg viewBox="0 0 1024 1024" width="1em" height="1em">
+      <path
+        d="M483.072 714.496l30.165333 30.208 415.957334-415.829333a42.837333 42.837333 0 0 0 0-60.288 42.538667 42.538667 0 0 0-60.330667-0.042667l-355.541333 355.413333-355.242667-355.413333a42.496 42.496 0 0 0-60.288 0 42.837333 42.837333 0 0 0-0.085333 60.330667l383.701333 383.872 1.706667 1.749333z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+
   const handleRenderSorter = ({
     activeAsc,
     activeDesc,
@@ -74,24 +87,16 @@ const App = () => {
   }) => {
     return (
       <>
-        <div
-          style={{
-            ...commonStyle,
-            color: activeAsc ? '#197afa' : '#dbdbdb',
-          }}
+        <Icon
+          component={UpArrow}
           onClick={triggerAsc}
-        >
-          {'>'}
-        </div>
-        <div
-          style={{
-            ...commonStyle,
-            color: activeDesc ? '#197afa' : '#dbdbdb',
-          }}
+          style={{ color: activeAsc ? '#197afa' : '#dbdbdb', fontSize: '12px', cursor: 'pointer' }}
+        />
+        <Icon
+          component={DownArrow}
           onClick={triggerDesc}
-        >
-          {'<'}
-        </div>
+          style={{ color: activeDesc ? '#197afa' : '#dbdbdb', fontSize: '12px', cursor: 'pointer' }}
+        />
       </>
     );
   };
