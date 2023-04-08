ab77d864871d22caa905b1b3081aa750dc71610b
guotingting <1531297152@qq.com>
2023-04-08 16:18:59 +0800

    chore: 删除无用代码

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,6 +1,3 @@
-// 在Mac下，滚动条是不占位的，所以宽度始终是为0的，在windows系统下，是占位的。所以取了个大约值16
-export const BAR_WIDTH = 16;
-
 export const BAR_THUMB_SIZE = 16;
 
 export const SELECTION_EXPAND_COLUMN_WIDTH = 44;
@@ -32,11 +29,3 @@ export const CLASS_SCROLLBAR_TRACK = `${PREFIXCLS}-scrollbar-track`;
 export const CLASS_SCROLLBAR_TRACK_SCROLLING = `${PREFIXCLS}-scrollbar-track-scrolling`;
 
 export const CLASS_SCROLLBAR_TRACK_ACTIVE = `${PREFIXCLS}-scrollbar-track-active`;
-
-export const omitColumnProps = [
-  '_width',
-  '_columnKey',
-  '_lastLeftFixed',
-  '_firstRightFixed',
-  '_ignoreRightBorder',
-];
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,7 +1,6 @@
 import type React from 'react';
 import omit from 'omit.js';
 import type {
-  RowKeyType,
   ColumnGroupType,
   ColumnsType,
   ColumnType,
@@ -48,44 +47,6 @@ export function omitColumnProps<T>(column: PrivateColumnGroupType<T> | PrivateCo
   ]);
 }
 
-// todo 待测试如果用的数据不存在的字段 record.desc  desc 不存在
-export function getRowKey<T>(rowKey: RowKeyType<T>, rowData: T) {
-  let key;
-  if (typeof rowKey === 'string') {
-    key = rowData[rowKey as keyof T] as string;
-  } else if (typeof rowKey === 'function') {
-    key = rowKey(rowData);
-  }
-
-  if (key === undefined) {
-    // key = i;
-    // console.error(
-    //   `Each record should have a unique "key" prop,or set "rowKey" to an unique primary key.Already converted with ${i}`,
-    // );
-    console.error(
-      `Each record should have a unique "key" prop,or set "rowKey" to an unique primary key.`,
-    );
-  }
-  return key;
-}
-
-export function findParentByKey<T extends { key?: number | string; children?: T[] }>(
-  data: T[] = [],
-  currKey: string | number,
-  rowKey: RowKeyType<T>,
-  parent?: T,
-): T | undefined {
-  for (let i = 0; i < data.length; i++) {
-    const curr = data[i];
-    const key = getRowKey(rowKey, curr);
-    if (key === currKey) return parent;
-    if (curr?.children && curr.children.length) {
-      const res = findParentByKey(curr.children, currKey, rowKey, curr);
-      if (res) return res;
-    }
-  }
-}
-
 export function omitRowsProps<T>(data: T[] | T = []) {
   const arr: any = [];
   data = Array.isArray(data) ? data : [data];
@@ -97,27 +58,6 @@ export function omitRowsProps<T>(data: T[] | T = []) {
     arr.push(obj);
   });
   return arr;
-}
-
-export function parseValue(value: string | number | undefined | null) {
-  if (typeof value === 'number') return value;
-  if (typeof value === 'string') {
-    if (/^\d+(?:px)?$/.test(value)) {
-      return parseInt(value, 10);
-    } else {
-      return value;
-    }
-  }
-  return null;
-}
-
-export function toPoint(value: string | number | null) {
-  if (typeof value === 'number') return value;
-  if (typeof value === 'string') {
-    const val = value.replace('%', '');
-    return Number(val) / 100;
-  }
-  return null;
 }
 
 export function getParent(el: HTMLElement, target: HTMLElement | string | null) {
@@ -153,104 +93,3 @@ export function extractPixel(value?: string) {
   }
   return 0;
 }
-
-export function getScrollbarWidth() {
-  const outer = document.createElement('div');
-  outer.style.visibility = 'hidden';
-  outer.style.width = '100px';
-  outer.style.position = 'absolute';
-  outer.style.top = '-9999px';
-  document.body.appendChild(outer);
-
-  const widthNoScroll = outer.offsetWidth;
-  outer.style.overflow = 'scroll';
-
-  const inner = document.createElement('div');
-  inner.style.width = '100%';
-  outer.appendChild(inner);
-
-  const widthWithScroll = inner.offsetWidth;
-  outer.parentNode!.removeChild(outer);
-
-  return widthNoScroll - widthWithScroll;
-}
-
-// const getChildrenKeys = (data: T[] = [], all = true) => {
-//   const keys: (number | string)[] = [];
-//   data.map((c) => {
-//     keys.push(c.rowKey);
-//     if (c?.children && c.children.length && all) {
-//       keys.push(...getChildrenKeys(c.children));
-//     }
-//   });
-//   return keys;
-// };
-//
-// const findParentByKey = (data: T[] = [], key: string | number) => {
-//   let item: undefined | T;
-//   for (let i = 0; i < data.length; i++) {
-//     const curr = data[i];
-//     if (curr.rowKey === key) {
-//       item = curr;
-//       break;
-//     }
-//     if (curr?.children && curr.children.length) {
-//       const res = findParentByKey(curr.children, key);
-//       if (res) {
-//         item = res;
-//         break;
-//       }
-//     }
-//   }
-//   return item;
-// };
-//
-// const getSelectParent = (
-//   parentKey: string | number,
-//   selectKeys: (string | number)[],
-//   currSelectedKey: number | string,
-// ) => {
-//   const arr: T[] = [];
-//   const parent = findParentByKey(dataSource, parentKey);
-//   if (!parent) return arr;
-//   const childKeys = getChildrenKeys(parent?.children, false);
-//   const exist = childKeys.filter((cKey) => selectKeys.indexOf(cKey) >= 0);
-//   if (exist.length + 1 === childKeys.length || (childKeys.length === 1 && !exist.length)) {
-//     arr.push(parent);
-//     if (parent?.parentKey) {
-//       arr.push(
-//         ...getSelectParent(parent.parentKey, [...selectKeys, currSelectedKey], parent.rowKey),
-//       );
-//     }
-//   }
-//   return arr;
-// };
-//
-// export function getSelectedItems(data: T[] = [], currSelectedKey?: number | string) {
-//   const arr: T[] = [];
-//
-//   for (let i = 0; i < data.length; i++) {
-//     const d = data[i];
-//     const key = d.rowKey;
-//     if (currSelectedKey) {
-//       if (currSelectedKey === key) {
-//         arr.push(d);
-//         const childrenData = getSelectedItems(d?.children);
-//         if (childrenData.length) {
-//           arr.push(...childrenData);
-//         }
-//         if (d?.parentKey) {
-//           arr.push(...getSelectParent(d.parentKey, selectedKeys, currSelectedKey));
-//         }
-//       }
-//     } else {
-//       arr.push(d);
-//       const childrenData = getSelectedItems(d?.children);
-//       if (childrenData.length) {
-//         arr.push(...childrenData);
-//       }
-//     }
-//   }
-//
-//   return arr;
-// }
