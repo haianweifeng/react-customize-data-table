3dd4b6fa6219f05ae67a01f12d2a02df86d6a583
guotingting <1531297152@qq.com>
2023-05-10 11:22:10 +0800

    feat: 修改配置

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,4 +1,8 @@
 export default {
   // more father 4 config: https://github.com/umijs/father-next/blob/master/docs/config.md
-  esm: {},
+  platform: 'browser',
+  cjs: { output: 'lib' },
+  esm: {
+    output: 'es',
+  },
 };
