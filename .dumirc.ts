489d75117800a0f29d8af772ac49e199cd78b4ff
guotingting <1531297152@qq.com>
2023-05-13 15:23:41 +0800

    feat: 修改配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-data-table' : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/react-data-table' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-data-table/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/react-data-table/' : '/',
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
     name: 'react-data-table',
