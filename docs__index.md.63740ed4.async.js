7e9b19b4872f2fc4d0c248b6832ba92d9465bac4
haianweifeng <1531297152@qq.com>
2023-05-23 07:34:39 +0000

    Updates

---
+++ docs__index.md.63740ed4.async.js
@@ -0,0 +1,5 @@
+"use strict";(self.webpackChunkreact_customize_data_table=self.webpackChunkreact_customize_data_table||[]).push([[6935],{2511:function(i,a,e){e.r(a);var l=e(71100),u=e(35532),r=e(19532),o=e(99900),m=e(60037),c=e(67915),h=e(34243),n=e(14570),E=e(67294),_=e(85893);function d(){var s=(0,n.eL)(),t=s.texts;return(0,_.jsx)(n.dY,{children:(0,_.jsx)(_.Fragment,{children:(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsxs)("h2",{id:"example",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#example",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"Example"]}),(0,_.jsx)("p",{children:(0,_.jsx)("a",{href:"http://localhost:8000",children:t[0].value})}),(0,_.jsxs)("p",{children:[t[1].value,(0,_.jsx)("a",{href:"https://haianweifeng.github.io/react-customize-data-table",children:t[2].value})]}),(0,_.jsxs)("h2",{id:"license",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#license",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"License"]}),(0,_.jsx)("p",{children:t[3].value}),(0,_.jsx)("style",{dangerouslySetInnerHTML:{__html:`
+.dumi-default-hero-title {
+    font-size: 72px;
+}
+`}})]})})})}a.default=d}}]);
