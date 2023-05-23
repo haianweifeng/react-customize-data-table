1fa9b9687a1e4cd767ad5e11ec622cac7cd4c56e
haianweifeng <1531297152@qq.com>
2023-05-23 15:03:50 +0000

    Updates

---
+++ docs__demo__style-events.en-US.md.939f0d57.async.js
@@ -0,0 +1,35 @@
+"use strict";(self.webpackChunkreact_customize_data_table=self.webpackChunkreact_customize_data_table||[]).push([[4766],{95860:function(r,a,t){t.r(a);var s=t(71100),m=t(35532),c=t(19532),i=t(73868),o=t(60037),u=t(67915),f=t(34243),_=t(14570),b=t(67294),e=t(85893);function l(){var d=(0,_.eL)(),n=d.texts;return(0,e.jsx)(_.dY,{children:(0,e.jsxs)(e.Fragment,{children:[(0,e.jsx)(_.Dl,{demo:{id:"docs-demo-style-events-en-us-demo-rowstyle"},previewerProps:{title:"Custom row styles",filename:"docs/examples/RowStyle.tsx"}}),(0,e.jsx)("div",{className:"markdown",children:(0,e.jsx)(o.Z,{lang:"css",children:n[0].value})}),(0,e.jsx)(_.Dl,{demo:{id:"docs-demo-style-events-en-us-demo-columnstyle"},previewerProps:{title:"Custom column styles",filename:"docs/examples/ColumnStyle.tsx"}}),(0,e.jsx)("div",{className:"markdown",children:(0,e.jsx)(o.Z,{lang:"css",children:n[1].value})}),(0,e.jsx)(_.Dl,{demo:{id:"docs-demo-style-events-en-us-demo-cellstyle"},previewerProps:{title:"Custom cell styles",filename:"docs/examples/CellStyle.tsx"}}),(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsx)(o.Z,{lang:"css",children:n[2].value}),(0,e.jsx)("style",{dangerouslySetInnerHTML:{__html:`
+.rv-table-container .demo-table-info-row td,
+.rv-table-container .demo-table-info-row:hover td {
+   background-color: #2db7f5;
+   color: #fff;
+}
+
+.rv-table-container .demo-table-error-row td,
+.rv-table-container .demo-table-error-row:hover td {
+    background-color: #ff6600;
+    color: #fff;
+}
+
+.rv-table-container td.demo-table-info-column,
+.rv-table-container tr:hover td.demo-table-info-column {
+    background-color: #2db7f5;
+    color: #fff;
+}
+
+.rv-table-container tr td.demo-table-info-cell-name,
+.rv-table-container tr:hover td.demo-table-info-cell-name {
+    background-color: #2db7f5;
+    color: #fff;
+}
+.rv-table-container tr td.demo-table-info-cell-age,
+.rv-table-container tr:hover td.demo-table-info-cell-age {
+    background-color: #ff6600;
+    color: #fff;
+}
+.rv-table-container tr td.demo-table-info-cell-address,
+.rv-table-container tr:hover td.demo-table-info-cell-address {
+    background-color: #187;
+    color: #fff;
+}
+`}})]}),(0,e.jsx)(_.Dl,{demo:{id:"docs-demo-style-events-en-us-demo-events"},previewerProps:{title:"Events",filename:"docs/examples/Events.tsx"}})]})})}a.default=l}}]);
